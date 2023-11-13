8228fb798e6d94c000f15f1115d2ea1efa73d759
ChristianBautista1708 <christianbautista_2226250002@mhs.mdp.ac.id>
2023-11-13 17:56:32 +0700

    Navigasi

---
+++ wisata_candi/lib/screens/favorite_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/home_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
+
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
